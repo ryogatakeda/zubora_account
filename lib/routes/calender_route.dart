0b1c7921578d488d93c6299476cdedce49047533
kanta218 <guantaizhongze2@gmail.com>
2020-11-28 16:05:23 +0900

    no message

---
+++ lib/routes/calender_route.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Calender extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("カレンダー"),
+      ),
+      body: Center(
+        child: Text("カレンダー"),
+      ),
+    );
+  }
+}
---
+++ lib/routes/configuration_route.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Configuration extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("設定"),
+      ),
+      body: Center(
+        child: Text("設定"),
+      ),
+    );
+  }
+}
