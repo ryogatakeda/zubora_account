eaca60a5264c01ae87eb523cf99afc4c3161bedb
kanta218 <guantaizhongze2@gmail.com>
2020-11-28 17:28:15 +0900

    ベース修正

--- android/app/src/main/kotlin/com/example/kakeibo_20201122/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kakeibo_20201122
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ zubora/android/app/src/main/kotlin/com/example/zubora/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zubora
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
